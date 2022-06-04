c4d6496dfc23a228e8356de95c0086714e113e2e
yopox <yopoxdev@gmail.com>
2022-06-04 16:18:16 +0200

    n u p e s

--- core/src/main/kotlin/com/yopox/Nupes.kt
+++ core/src/main/kotlin/com/yopox/Nupes.kt
@@ -1,13 +1,13 @@
 package com.yopox
 
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.Texture.TextureFilter.Linear
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.viewport.FitViewport
+import com.yopox.ui.mrmoRegions
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.app.clearScreen
 import ktx.assets.disposeSafely
-import ktx.assets.toInternalFile
 import ktx.graphics.use
 
 class Nupes : KtxGame<KtxScreen>() {
@@ -18,18 +18,29 @@ class Nupes : KtxGame<KtxScreen>() {
 }
 
 class FirstScreen : KtxScreen {
-    private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
     private val batch = SpriteBatch()
+    private val camera = OrthographicCamera()
+    private val viewport = FitViewport(8f * 32, 8f * 20, camera)
 
     override fun render(delta: Float) {
-        clearScreen(red = 0.7f, green = 0.7f, blue = 0.7f)
+        batch.projectionMatrix = camera.combined
+        clearScreen(0f, 0f, 0f)
         batch.use {
-            it.draw(image, 100f, 160f)
+            val offset = 30f
+            it.draw(mrmoRegions[28][14], offset + 0 * 8f, 20f)
+            it.draw(mrmoRegions[28][21], offset + 1 * 8f, 20f)
+            it.draw(mrmoRegions[28][16], offset + 2 * 8f, 20f)
+            it.draw(mrmoRegions[28][5], offset + 3 * 8f, 20f)
+            it.draw(mrmoRegions[28][19], offset + 4 * 8f, 20f)
         }
     }
 
+    override fun resize(width: Int, height: Int) {
+        viewport.update(width, height, true)
+        camera.update()
+    }
+
     override fun dispose() {
-        image.disposeSafely()
         batch.disposeSafely()
     }
 }
---
+++ core/src/main/kotlin/com/yopox/ui/Tile.kt
@@ -0,0 +1,10 @@
+package com.yopox.ui
+
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+
+val mrmoTexture = Texture("MRMOTEXT.png")
+val mrmoRegions = TextureRegion.split(mrmoTexture, 8, 8)
+
+class Tile {
+}
