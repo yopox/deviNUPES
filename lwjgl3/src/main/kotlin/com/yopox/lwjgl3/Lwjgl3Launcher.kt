877b93e061e7a1b5864363f1663399586c05985c
yopox <yopoxdev@gmail.com>
2022-06-04 15:54:17 +0200

    Initial commit

---
+++ lwjgl3/src/main/kotlin/com/yopox/lwjgl3/Lwjgl3Launcher.kt
@@ -0,0 +1,16 @@
+@file:JvmName("Lwjgl3Launcher")
+
+package com.yopox.lwjgl3
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.yopox.Nupes
+
+/** Launches the desktop (LWJGL3) application. */
+fun main() {
+    Lwjgl3Application(Nupes(), Lwjgl3ApplicationConfiguration().apply {
+        setTitle("nupes")
+        setWindowedMode(640, 480)
+        setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
+    })
+}
