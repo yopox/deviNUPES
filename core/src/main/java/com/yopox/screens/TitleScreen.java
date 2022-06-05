172fea40b65cd2fcf6143c0f79300778e35890e6
yopox <yopoxdev@gmail.com>
2022-06-05 11:15:24 +0200

    Fix title

--- core/src/main/java/com/yopox/screens/TitleScreen.java
+++ core/src/main/java/com/yopox/screens/TitleScreen.java
@@ -13,9 +13,9 @@ import java.util.Vector;
 public class TitleScreen extends AbstractScreen {
     private static final Texture background = new Texture("title.png");
 
-    private Vector<Letter> option1 = Letter.listOf("Partie classique", Colors.DEFAULT_FG, Colors.DEFAULT_BG);
+    private Vector<Letter> option1 = Letter.listOf("Partie du jour", Colors.DEFAULT_FG, Colors.DEFAULT_BG);
     private Vector<Letter> option2 = Letter.listOf("5 minutes chrono", Colors.DEFAULT_FG, Colors.DEFAULT_BG);
-    private Vector<Letter> option3 = Letter.listOf("1 minute  chrono", Colors.DEFAULT_FG, Colors.DEFAULT_BG);
+    private Vector<Letter> option3 = Letter.listOf("Mode duel", Colors.DEFAULT_FG, Colors.DEFAULT_BG);
 
     private Vector<Letter> arrow = Letter.listOf(">", Colors.DEFAULT_FG, Colors.DEFAULT_BG);
     private final int N_OPTIONS = 3;
@@ -30,10 +30,10 @@ public class TitleScreen extends AbstractScreen {
         super.render(delta);
         myBatch.begin();
         myBatch.draw(background, 0f, 0f);
-        Text.draw(option1, Util.TILE * 12, Util.TILE * 8, false, myBatch);
-        Text.draw(option2, Util.TILE * 12, Util.TILE * 6, false, myBatch);
-        Text.draw(option3, Util.TILE * 12, Util.TILE * 4, false, myBatch);
-        Text.draw(arrow, Util.TILE * 9, Util.TILE * 8 - selected * 2 * Util.TILE, false, myBatch);
+        Text.draw(option1, Util.TILE * 13, Util.TILE * 8, false, myBatch);
+        Text.draw(option2, Util.TILE * 13, Util.TILE * 6, false, myBatch);
+        Text.draw(option3, Util.TILE * 13, Util.TILE * 4, false, myBatch);
+        Text.draw(arrow, Util.TILE * 10, Util.TILE * 8 - selected * 2 * Util.TILE, false, myBatch);
         myBatch.end();
     }
 
